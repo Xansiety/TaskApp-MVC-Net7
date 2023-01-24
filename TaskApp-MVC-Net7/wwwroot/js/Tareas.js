f7f8cd2667853ced54028b77ec5a66f4e8fe4eb0
xansiety <ferando543@outlook.com>
2023-01-23 20:52:39 -0600

    Instalacion knockout.js y primeros pasos

---
+++ TaskApp-MVC-Net7/wwwroot/js/Tareas.js
@@ -0,0 +1,17 @@
+﻿
+
+function agregarNuevaTareaAlListado() {
+    tareasListadoViewModel.tareas.push(new tareaElementoListadoViewModel({ id: 0, titulo: '' }));
+    $("[name=titulo-tarea]").last().focus();
+}
+
+function manejarFocusOutTituloTarea(tarea) {
+
+    const titulo = tarea.titulo();
+    if (!titulo) {
+        tareasListadoViewModel.tareas.pop();
+        return
+    }
+
+    tarea.id(1)
+}
